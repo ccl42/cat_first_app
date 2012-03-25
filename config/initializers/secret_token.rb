20f70db48942869cf7d12c26dcf6fc36e1e31e9c
ccl42 <cat@3vbtv.com>
2012-03-25 15:14:47 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CatFirstApp::Application.config.secret_token = '2340ded9b3616db2385b8341e36ddeb12949aece14d8b4fb072c1192fb28d0a8afc602b73b97aac52450d03ea22f000268ea7e76b52ba9fc192f7454d86e07ce'
